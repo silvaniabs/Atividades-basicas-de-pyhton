26c4282e2fd671c79c6e4e5f2ca8fa7ea52be89f
silvaniabs <silvania_barbosa_santos@yahoo.com>
2024-05-29 20:59:43 -0300

    enviando Atividades iniciais

---
+++ Desafio 02.py
@@ -0,0 +1,6 @@
+print('===== DESAFIO 02 =====')
+print('Informe os dados a seguir:')
+dia = input('Dia= ')
+mes = input('Mês= ')
+ano = input('Ano= ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
